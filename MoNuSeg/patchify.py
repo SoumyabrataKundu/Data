db8d00d8aba368614567eb553b3b09c7777ed806
SoumyabrataKundu <soumyabratakundu@uchicago.edu>
2025-06-06 21:10:16 -0500

    Update

--- MoNuSeg/patchify.py
+++ MoNuSeg/patchify.py
@@ -3,20 +3,14 @@ import h5py
 import sys
 sys.path.append('../../Steerable')
 
-from Steerable.datasets.hdf5 import HDF5, HDF5Dataset
-from Steerable.Segmentation.Patchify import PatchifyDataset
-
-def get_datasets() -> dict:
-    train_dataset = HDF5(h5py.File('data/MoNuSeg.hdf5', 'r'), mode='train')
-    test_dataset = HDF5(h5py.File('data/MoNuSeg.hdf5', 'r'), mode='test')
-
-    return {'train' : train_dataset, 'test' : test_dataset}
+from Steerable.utils import HDF5, HDF5Dataset, PatchifyDataset
 
 def main():
-    datasets = get_datasets()
-    hdf5file = HDF5Dataset('MoNuSeg_patched256_256.hdf5')
-    for mode in datasets:
-        hdf5file.create_hdf5_dataset(mode, PatchifyDataset(datasets[mode], kernel_size=(256,256), stride=256), batched=True)
+    data_file = h5py.File('data/MoNuSeg.hdf5', 'r')
+    hdf5file = HDF5Dataset('MoNuSeg_patched256_128.hdf5')
+
+    for mode in ['train', 'val', 'test']:
+        hdf5file.create_hdf5_dataset(mode, PatchifyDataset(HDF5(data_file, mode=mode), kernel_size=(256,256), stride=128), batched=True)
 
     return
 
--- PH2/patchify.py
+++ PH2/patchify.py
@@ -8,10 +8,9 @@ from Steerable.utils import HDF5, HDF5Dataset, PatchifyDataset
 def main():
     data_file = h5py.File('data/PH2.hdf5', 'r')
     datasets = {'train' : HDF5(data_file, mode='train'), 'val': HDF5(data_file, mode='val'), 'test' : HDF5(data_file, mode='test')}
-    hdf5file = HDF5Dataset('PH2_patched256_256.hdf5')
-    for mode in datasets:
-        if datasets[mode] is not None:
-            hdf5file.create_hdf5_dataset(mode, PatchifyDataset(datasets[mode], kernel_size=(256,256), stride=256), batched=True)
+    hdf5file = HDF5Dataset('PH2_patched256_128.hdf5')
+    for mode in ['train', 'val', 'test']:
+        hdf5file.create_hdf5_dataset(mode, PatchifyDataset(HDF5(data_file, mode=mode), kernel_size=(256,256), stride=128), batched=True)
 
     return
 
